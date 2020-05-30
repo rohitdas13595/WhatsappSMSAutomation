a829b2c25b8dd76ab03c3a82897865c731c8a274
rohitdas13595 <rohitdas10595@gmail.com>
2020-05-30 15:46:53 +0530

    modification

--- msg.py
+++ msg.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 account_sid = 'AC1ee4d176d6b662e56b89e9a1e136cd0e'
-auth_token = 'b1404c78cafe137a9a8f2bf4a68b0a68'
+auth_token = '3b6622fc220469011bb2747f6b9e79e9'
 client = Client(account_sid, auth_token)
 def send_notice():
     message = client.messages.create(
