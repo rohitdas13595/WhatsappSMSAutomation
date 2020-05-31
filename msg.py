53f85a70749a389805e958959c64a24db280f664
Rohit Kumar Das <rohitdas10595@gmail.com>
2020-05-31 11:46:10 +0530

    new update

--- msg.py
+++ msg.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = 'AC1ee4d176d6b662e56b89e9a1e136cd0e'
-auth_token = '3b6622fc220469011bb2747f6b9e79e9'
+account_sid = 'Your Twilio Project ID'
+auth_token = 'Your project auth token'
 client = Client(account_sid, auth_token)
 def send_notice():
     message = client.messages.create(
